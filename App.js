4a74103ce318dcb124d489c4b737cbd8b7db898d
ayodhya98 <75997356+ayodhya98@users.noreply.github.com>
2022-07-22 15:49:28 +0530

    Add files via upload
    react native login

---
+++ App.js
@@ -0,0 +1,11 @@
+import { View } from 'react-native'
+import React from 'react'
+import { Input, NativeBaseProvider,Text,Slider,Box,VStack,Button} from 'native-base';
+import SingnUp from './Pages/SingnUp';
+
+export default function App() {
+  return (
+    <SingnUp/>
+   
+  )
+}
